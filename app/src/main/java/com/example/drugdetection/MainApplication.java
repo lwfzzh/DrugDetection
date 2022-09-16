ec384cd467e6926f115f560fa86165ffb1ad9eb6
Healvet <1519296797@qq.com>
2022-09-16 13:50:58 +0800

    毒品检测登录页和编辑个人信息页面

---
+++ app/src/main/java/com/example/drugdetection/MainApplication.java
@@ -0,0 +1,18 @@
+package com.example.drugdetection;
+
+import android.app.Application;
+import android.content.Context;
+
+public class MainApplication extends Application {
+    private static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = getApplicationContext();
+    }
+
+    public static Context getCustomApplicationContext() {
+        return mContext;
+    }
+}
